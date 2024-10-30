8a77af908c00618cbca1ffabb136ddb74169ec22
qinhuangdao-home <431225046@qq.com>
2024-10-30 11:15:54 +0800

    up

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/hex"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -12,18 +13,23 @@ import (
 	"time"
 )
 
+var (
+	wk            string
+	maxParallel   int
+	verbose       bool
+	singleTimeout int
+)
+
 func main() {
 	var ir string
-	var wk string
-	var maxParallel int
-	var verbose bool
 	var only bool
 	var mp4 bool
-	var singleTimeout int
+	var mp4fn string
 	var urPrefix string
 	flag.StringVar(&ir, "i", "", "m3u8 url or file")
 	flag.StringVar(&wk, "wk", "m3u8cache", "work dir")
 	flag.StringVar(&urPrefix, "up", "", "m3u8 url prefix")
+	flag.StringVar(&mp4fn, "mfn", "out", "mp4 file name")
 	flag.IntVar(&maxParallel, "mp", 5, "max parallel")
 	flag.IntVar(&singleTimeout, "st", 5, "single request timeout(seconds)")
 	flag.BoolVar(&verbose, "v", false, "verbose")
@@ -32,55 +38,100 @@ func main() {
 	flag.Parse()
 	if ir == "" {
 		log.Printf("m3u8 url or file not be empty\n")
-		return
+		os.Exit(1)
 	}
+	_ = os.MkdirAll(wk, os.ModePerm)
+	if only {
+		err := onlyDown(ir)
+		if err != nil {
+			log.Printf("only download %v\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+	files, err := ParseDown(ir, urPrefix)
+	if err != nil {
+		log.Printf("parse down %v\n", err)
+		os.Exit(1)
+	}
+	err = MergeMp4(files, mp4fn, mp4)
+	if err != nil {
+		log.Printf("merge mp4 %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func onlyDown(ir string) error {
+	prefix, err := UriPrefix(ir)
+	if err != nil {
+		return fmt.Errorf("UriPrefix %v", err)
+	}
+	_, fn, _ := strings.Cut(ir, prefix+"/")
+	if fn != "" {
+		err = DownloadFile(ir, wk+"/"+fn, time.Duration(singleTimeout)*time.Second)
+		if err != nil {
+			return fmt.Errorf("DownloadFile %v %v", ir, err)
+		}
+	}
+	return nil
+}
+
+func getInputBytes(ir, urPrefix string) (m3u8bs []byte, prefix string, err error) {
 	var m3u8f string
-	var err error
 	if !IsHttp(ir) {
 		if !FileIsExist(ir) {
-			log.Printf("m3u8 file not exist\n")
+			err = fmt.Errorf("m3u8 file not exist")
 			return
 		}
 		m3u8f = ir
-		if urPrefix == "" {
-			log.Printf("m3u8 url prefix not be empty\n")
+		prefix = urPrefix
+		if prefix == "" {
+			err = fmt.Errorf("m3u8 url prefix not be empty")
 			return
 		}
 	} else {
-		urPrefix, err = UriPrefix(ir)
+		prefix, err = UriPrefix(ir)
 		if err != nil {
-			log.Printf("UriPrefix %v\n", err)
+			err = fmt.Errorf("UriPrefix %v", err)
 			return
 		}
 	}
-	_ = os.MkdirAll(wk, os.ModePerm)
-	if only && m3u8f == "" {
-		_, fn, _ := strings.Cut(ir, urPrefix+"/")
-		if fn != "" {
-			err = DownloadFile(ir, wk+"/"+fn, 0)
-			if err != nil {
-				log.Printf("DownloadFile %v %v\n", ir, err)
-			}
-		}
-		return
-	}
-	var m3u8bs []byte
 	if m3u8f == "" {
 		m3u8bs, err = DownloadFileBytes(ir, time.Duration(singleTimeout)*time.Second)
-		if err != nil {
-			log.Printf("DownloadFileBytes %v\n", err)
-			return
-		}
 	} else {
 		m3u8bs, err = os.ReadFile(m3u8f)
-		if err != nil {
-			log.Printf("ReadFileBytes %v\n", err)
-			return
-		}
 	}
-	m3u8, err := ParseM3u8(m3u8bs, urPrefix)
+	return
+}
+
+func MergeMp4(files, out string, flag bool) error {
+	fcp := []string{"-f", "concat", "-safe", "0",
+		"-i", files, "-c", "copy", out + ".mp4"}
+	fcpj := strings.Join(fcp, " ")
+	if flag {
+		log.Printf("ffmpeg %v\n", fcpj)
+		command := exec.Command("ffmpeg", fcp...)
+		command.Stdout = os.Stdout
+		err := command.Run()
+		if err != nil {
+			return err
+		} else {
+			time.Sleep(time.Second)
+			_ = os.RemoveAll(wk)
+		}
+	} else {
+		log.Printf("please run\nffmpeg %v\n", fcpj)
+	}
+	return nil
+}
+
+func ParseDown(ir, urPrefix string) (files string, err error) {
+	m3u8bs, prefix, err := getInputBytes(ir, urPrefix)
 	if err != nil {
-		log.Printf("ParseM3u8 %v\n", err)
+		return
+	}
+	m3u8, err := ParseM3u8(m3u8bs, prefix)
+	if err != nil {
 		return
 	}
 	if len(m3u8.Key) != 0 {
@@ -92,12 +143,12 @@ func main() {
 	}
 	loader := NewLoader(maxParallel, verbose)
 	fisn := wk + "/files.txt"
-	files, err := os.OpenFile(fisn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	var filestxt *os.File
+	filestxt, err = os.OpenFile(fisn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	if err != nil {
-		log.Printf("create files %v\n", err)
 		return
 	}
-	defer files.Close()
+	defer filestxt.Close()
 	fsm := make(map[int]string)
 	for i, t := range m3u8.Tss {
 		fn := wk + "/" + t.Name + ".ts"
@@ -136,33 +187,17 @@ func main() {
 	log.Printf("download succNum: %v errNum: %v cost: %s\n", succ, errn, cost)
 	ln := len(fsm)
 	if ln == 0 {
+		err = errors.New("ts file list be empty")
 		return
 	}
 	for i := 0; i < ln; i++ {
-		_, _ = files.WriteString(fsm[i])
+		_, _ = filestxt.WriteString(fsm[i])
 	}
-	abs, err := filepath.Abs(fisn)
+	files, err = filepath.Abs(fisn)
 	if err != nil {
-		log.Printf("file abs %v %v\n", fisn, abs)
-		return
+		err = fmt.Errorf("file abs %v %v", fisn, err)
 	}
-	fcp := []string{"-f", "concat", "-safe", "0",
-		"-i", abs, "-c", "copy", "out.mp4"}
-	fcpj := strings.Join(fcp, " ")
-	if mp4 {
-		log.Printf("ffmpeg %v\n", fcpj)
-		command := exec.Command("ffmpeg", fcp...)
-		command.Stdout = os.Stdout
-		err = command.Run()
-		if err != nil {
-			log.Printf("exec ffmpeg %v\n", err)
-		} else {
-			time.Sleep(time.Second)
-			_ = os.RemoveAll(wk)
-		}
-	} else {
-		log.Printf("please run\nffmpeg %v\n", fcpj)
-	}
+	return
 }
 
 func FileIsExist(fn string) bool {
